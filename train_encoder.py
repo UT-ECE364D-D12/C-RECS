Add gradient accumulation, Fix validation loss calculations

train_encoder.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import torch
+import yaml
 from sklearn.model_selection import train_test_split
 from torch import optim
 from torch.utils.data import DataLoader
@@ -11,6 +12,8 @@ from model.recommender import DeepFM
 from utils.data import EncoderDataset, get_feature_sizes, train_test_split_requests
 from utils.loss import EncoderRecommenderCriterion
 from utils.processor import train_encoder
+
+args = yaml.safe_load(open("configs/encoder.yaml", "r"))
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -29,23 +32,26 @@ train_ratings, test_ratings = train_test_split(ratings, train_size=0.8)
 
 train_dataset, test_dataset = EncoderDataset(train_ratings, train_requests), EncoderDataset(test_ratings, test_requests)
 
-train_dataloader, test_dataloader = DataLoader(train_dataset, batch_size=80, shuffle=True, num_workers=4, drop_last=True), DataLoader(test_dataset, batch_size=80, num_workers=4, drop_last=True)
+train_dataloader, test_dataloader = DataLoader(train_dataset, batch_size=args["batch_size"], shuffle=True, num_workers=4, drop_last=True), DataLoader(test_dataset, batch_size=args["batch_size"], num_workers=4, drop_last=True)
 
 encoder = Encoder().to(device)
 
-recommender = DeepFM(feature_dims=get_feature_sizes(ratings), embed_dim=768, mlp_dims=(16, 16), dropout=0.8).to(device)
+recommender = DeepFM(feature_dims=get_feature_sizes(ratings), embed_dim=768, mlp_dims=(768, 768), dropout=0.8).to(device)
 
 expander = build_expander(embed_dim=768, width=2).to(device)
 
 classifier = build_classifier(embed_dim=768, num_classes=requests["movie_id"].nunique()).to(device)
 
-optimizer = optim.AdamW(list(encoder.parameters()) + list(expander.parameters()) + list(classifier.parameters()) + list(recommender.parameters()), lr=0.0001)
+optimizer = optim.AdamW([
+    {"params": encoder.parameters(), **args["optimizer"]["encoder"]},
+    {"params": expander.parameters(), **args["optimizer"]["expander"]},
+    {"params": classifier.parameters(), **args["optimizer"]["classifier"]},
+    {"params": recommender.parameters(), **args["optimizer"]["recommender"]},
+])
 
-loss_weights = {"triplet": 1.0, "mse": 1.0, "id": 0.5, "variance": 0.8, "invariance": 0.8, "covariance": 0.008}
+criterion = EncoderRecommenderCriterion(expander, classifier, loss_weights=args["loss_weights"])
 
-criterion = EncoderRecommenderCriterion(expander, classifier, loss_weights=loss_weights)
-
-wandb.init(project="MovieLens", name="Joint Training", tags=("Encoder",), config={"model": "Encoder", "optimizer": "AdamW", "lr": 0.0001, "loss_weights": loss_weights, "batch_size": 80})
+wandb.init(project="MovieLens", name="Rec + Encoder + VICReg", tags=("Encoder",), config=args)
 
 train_encoder(
     encoder=encoder,
@@ -54,10 +60,11 @@ train_encoder(
     criterion=criterion,
     train_dataloader=train_dataloader,
     test_dataloader=test_dataloader,
-    max_epochs=10,
-    device=device
+    device=device,
+    **args["train"]
 )
 
 wandb.finish()
 
+torch.save(recommender.state_dict(), "weights/recommender/deepfm.pt")
 torch.save(encoder.state_dict(), "weights/encoder/encoder.pt")