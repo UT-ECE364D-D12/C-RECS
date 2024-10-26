361bbef6eadf37cd80798943ca1002a027e6baf4
troydutton <troydutton@utexas.edu>
2024-10-26 05:09:46 -0500

    Add prediction function

--- model/recommender.py
+++ model/recommender.py
@@ -15,12 +15,11 @@ class DeepFM(nn.Module):
     def __init__(self, num_items: int, embed_dim: int, mlp_dims: List[int], dropout: float, weights: str = None):
         super().__init__()
 
-        self.embedding = FeaturesEmbedding(num_items, embed_dim)
-        
-        self.linear = FeaturesLinear(num_items, output_dim=1)
-        
-        self.fm = FactorizationMachine()
+        self.num_items = num_items
 
+        self.embedding = FeaturesEmbedding(num_items, embed_dim)
+        self.linear = FeaturesLinear(num_items, output_dim=1)
+        self.fm = FactorizationMachine()
         self.mlp = MultiLayerPerceptron(input_dim=2 * embed_dim, hidden_dims=mlp_dims, output_dim=1, dropout=dropout)
 
         if weights is not None:
@@ -39,3 +38,13 @@ class DeepFM(nn.Module):
         mlp_term = self.mlp(embeddings.flatten(1, 2))
 
         return torch.sigmoid(linear_term + fm_term + mlp_term).squeeze(1)
+
+    def predict(self, features: Tuple[Tensor, Tensor]) -> Tensor:
+        feature_ids, feature_ratings = features
+
+        with torch.no_grad():
+            item_ids = torch.arange(self.num_items, device=feature_ids.device)
+            feature_ids = feature_ids.repeat(self.num_items).split(len(feature_ids))
+            feature_ratings = feature_ratings.repeat(self.num_items).split(len(feature_ratings))
+
+            return self.forward((feature_ids, feature_ratings, item_ids))
