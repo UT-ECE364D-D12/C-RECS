23b60df4b62a5e5a6e708a8b36f957b73a5a1281
troydutton <troydutton@utexas.edu>
2024-12-05 00:11:18 -0600

    Revert to BERT

--- model/encoder.py
+++ model/encoder.py
@@ -1,33 +1,34 @@
-from utils.misc import send_to_device
 import logging
 from typing import List
 
 import torch
 from torch import Tensor, nn
+from transformers import BertModel, BertTokenizer
 
 from model.layers import MultiLayerPerceptron
-from sentence_transformers import SentenceTransformer
+
 
 class Encoder(nn.Module):
-    def __init__(self, model_name: str = "all-mpnet-base-v2", weights: str = None, **kwargs) -> None:
+    def __init__(self, model_name: str = "bert-base-uncased", weights: str = None, **kwargs) -> None:
         super().__init__()
         loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
         for logger in loggers:
             if "transformers" in logger.name.lower():
                 logger.setLevel(logging.ERROR)
                 
-        self.model = SentenceTransformer(model_name, config_kwargs=kwargs)
-
-        self.embed_dim = self.model.get_sentence_embedding_dimension()
+        self.tokenizer: BertTokenizer = BertTokenizer.from_pretrained(model_name, clean_up_tokenization_spaces=True)
+        self.model: BertModel = BertModel.from_pretrained(model_name, **kwargs)
+        self.embed_dim = self.model.config.hidden_size
         
         if weights is not None:
             self.load_state_dict(torch.load(weights, weights_only=True))
     
     def forward(self, requests: List[str]) -> Tensor:
-        
-        request_tokens = send_to_device(self.model.tokenize(requests), device=self.model.device)
+        encoder_tokens = self.tokenizer(requests, padding=True, truncation=True, max_length=512, return_tensors="pt").to(self.model.device)
 
-        encoded_requests = self.model(request_tokens)["sentence_embedding"]
+        batch_encoded_requests = self.model(**encoder_tokens)
+
+        encoded_requests = batch_encoded_requests.last_hidden_state[:, 0]
 
         return encoded_requests
     
