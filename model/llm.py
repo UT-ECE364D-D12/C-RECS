f0223a618cb947ad49adf6025887be4d7eb30184
troydutton <troydutton@utexas.edu>
2025-01-03 13:38:13 -0600

    Add support for unquantized llms

--- model/llm.py
+++ model/llm.py
@@ -2,29 +2,36 @@ from utils.misc import suppress_warnings
 
 suppress_warnings()
 
+from importlib.util import find_spec
+
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 
 def build_language_model(model_name: str = "google/gemma-7b-it", quantize: str = "8bit") -> tuple[AutoModelForCausalLM, AutoTokenizer]:
+    # If bitsandbytes is not installed, quantization is not possible.
+    if find_spec("bitsandbytes") is None:
+        quantize = None
 
-    if quantize == "4bit":
-        config = BitsAndBytesConfig(
+    if quantize is None:
+        quantization_config = None
+    elif quantize == "4bit":
+        quantization_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4",
             bnb_4bit_compute_dtype=torch.bfloat16,
         )
     elif quantize == "8bit":
-        config = BitsAndBytesConfig(load_in_8bit=True)
-    else: # Only support 4bit and 8bit quantization
-        raise ValueError("Invalid quantization type. Choose between '4bit' and '8bit'.")
+        quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+    else:
+        raise ValueError("Invalid quantization type. Choose between 'None', '4bit', and '8bit'.")
 
     model = AutoModelForCausalLM.from_pretrained(
         model_name, 
         device_map="auto",
         low_cpu_mem_usage=True,
-        quantization_config=config,
+        quantization_config=quantization_config,
         torch_dtype=torch.bfloat16,
         attn_implementation="flash_attention_2",
     )
